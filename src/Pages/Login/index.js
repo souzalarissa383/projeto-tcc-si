16b28a5f7d2ce3137e442be7f4a7dc2b4cf26c45
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2023-05-28 18:50:42 -0300

    Botao

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import { Form, Container } from "react-bootstrap";
-
+import React, {useState} from 'react'
+import { Form, Container } from "./style";
+import Input from "../../Components/Input/index";
+import Botao from "../../Components/Botao/index";
 const Login = () => {
-    return (
-        <div className="App">
-          <Container>
-            <Form>
-                <h1>Login!!!</h1>
-            </Form>
-          </Container>
-        </div>
-    )
-}
 
-export default Login
+    const handleSubmit = async () => {
+        alert('Login')
+    }
+
+    const handleChange = (event) =>{
+        console.log('Digitando....',event.target.name, event.targe.value)
+    }
+
+  return (
+    <div className="App">
+      <Container>
+        <Form>
+          <Input
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={handleChange}
+            type="email"
+          />
+          <Input
+            name="password"
+            placeholder="Digite a sua senha"
+            onChange={handleChange}
+            type="password"
+          />
+          <Botao
+            type="submit"
+            text="Entrar!"
+            onClick={handleSubmit}
+          
+          />
+          <h1>Entre no seu perfil</h1>
+
+          <div>
+            <p>Não possui conta?</p>
+            <a>Cadastrar</a>
+          </div>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
+
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
   min-width: 100vw;
-  background-color: #383838
+  background-color: #282c34
 
 `
 
